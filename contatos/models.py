79ade9a2746bb756aebd660489d95d443bb8f4e2
WILDSON CAIO FELIPE <wildson.felipe@prof.sc.senac.br>
2023-05-04 21:59:27 -0300

    'CRUD'

--- contatos/models.py
+++ contatos/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Contatos(models.Model):
     nome = models.CharField(max_length=255)
+    imagem = models.ImageField()
     cpf = models.CharField(max_length=11)
     email = models.EmailField()
     telefone = models.IntegerField()
--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Contatos
 
 def index(request):
@@ -7,6 +7,33 @@ def index(request):
 
 def search(request):
     q = request.GET.get('search')
-    print(q)
     contatos = Contatos.objects.filter(nome__icontains=q)
     return render(request, 'pages/index.html', {'contatos':contatos})
+
+def detalhes(request, id):
+    contato = Contatos.objects.get(id=id)
+    return render(request, 'pages/detalhes.html', {'contato':contato})
+
+def deletar(request, id):
+    contato = Contatos.objects.get(id=id)
+    contato.delete()
+    return redirect('home')
+
+def adicionar(request):
+
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        cpf = request.POST.get('cpf')
+        email = request.POST.get('email')
+        altura = request.POST.get('altura')
+        descricao = request.POST.get('descricao')
+        data = request.POST.get('data_nasc')
+        telefone = request.POST.get('telefone')
+        imagem = request.FILES.get('imagem')
+        novo_contato = Contatos(nome=nome,cpf=cpf, email=email, altura=altura, descricao=descricao, data_nascimento=data, telefone=telefone, imagem=imagem, ativo=True)
+        novo_contato.save()
+        return redirect('home')
+
+
+    else:
+        return render(request, 'pages/adicionar.html')
