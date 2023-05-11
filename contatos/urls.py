54e9e3c084096a1dc8b226c16ac9d64618efa652
WILDSON CAIO FELIPE <wildson.felipe@prof.sc.senac.br>
2023-05-11 19:16:07 -0300

    'CRUD'

--- contatos/urls.py
+++ contatos/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('detalhes/<int:id>', views.detalhes, name='detalhes'),
     path('deletar/<int:id>', views.deletar, name='deletar'),
     path('adicionar/', views.adicionar, name='adicionar'),
+    path('editar/<int:id>', views.editar, name='editar'),
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from . models import Contatos
 
 def index(request):
-    contatos = Contatos.objects.all()
+    contatos = Contatos.objects.all().order_by('-id')
     return render(request, 'pages/index.html', {'contatos':contatos})
 
 def search(request):
@@ -33,7 +33,36 @@ def adicionar(request):
         novo_contato = Contatos(nome=nome,cpf=cpf, email=email, altura=altura, descricao=descricao, data_nascimento=data, telefone=telefone, imagem=imagem, ativo=True)
         novo_contato.save()
         return redirect('home')
-
-
     else:
         return render(request, 'pages/adicionar.html')
+
+
+def editar(request, id):
+    contato = Contatos.objects.get(id=id)
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        cpf = request.POST.get('cpf')
+        email = request.POST.get('email')
+        altura = request.POST.get('altura')
+        descricao = request.POST.get('descricao')
+        data = request.POST.get('data_nasc')
+        telefone = request.POST.get('telefone')
+        check = request.POST.get('check')
+        if check == None:
+            check = False
+        else:
+            check = True    
+        imagem = request.FILES.get('imagem')
+        contato.nome = nome
+        contato.cpf = cpf
+        contato.email = email
+        contato.telefone = telefone
+        contato.data = data
+        contato.imagem = imagem
+        contato.altura = altura
+        contato.descricao = descricao
+        contato.ativo = check
+        contato.save()
+        return redirect('home')
+    else:    
+        return render(request, 'pages/editar.html', {'contato':contato})
